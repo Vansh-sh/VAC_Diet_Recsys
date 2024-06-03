37623d831f1ec2216c3251c766fd58f8cb7d3d0b
Vansh Shah <149971403+Vansh-sh@users.noreply.github.com>
2024-06-03 23:45:59 +0530

    Add files via upload

---
+++ Rec_Sys/Streamlit_Frontend/Hello.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Diet Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
